6a41ed4a00739fd977477db17374985c8a4e7325
theocaulet <caulet.theo@gmail.com>
2026-03-09 11:27:23 +0100

    feat: enhance user registration by adding password hashing and improve user retrieval response

--- part3/app/api/v1/users.py
+++ part3/app/api/v1/users.py
@@ -7,7 +7,8 @@ api = Namespace('users', description='User operations')
 user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
-    'email': fields.String(required=True, description='Email of the user')
+    'email': fields.String(required=True, description='Email of the user'),
+    'password': fields.String(required=True, description='Password for the user')
 })
 
 
@@ -28,11 +29,9 @@ class UserList(Resource):
 
         try:
             new_user = facade.create_user(user_data)
-            return {'id': new_user.id,
-                    'first_name': new_user.first_name,
-                    'last_name': new_user.last_name,
-                    'email': new_user.email
-                    }, 201
+            new_user.hash_password(user_data['password'])
+            return {'id': new_user.id, 'message':
+                    'User successfully created'}, 201
         except ValueError as e:
             return {'error': str(e)}, 400
 
@@ -57,7 +56,8 @@ class UserResource(Resource):
         user = facade.get_user(user_id)
         if not user:
             return {'error': 'User not found'}, 404
-        return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+        return {'id': user.id, 'first_name': user.first_name,
+                'last_name': user.last_name, 'email': user.email}, 200
 
     @api.expect(user_model, validate=True)
     @api.response(200, 'User updated successfully')
