0c5615bd23e984f7c07fd5ec47bc3fb6ac69dbd0
théo caulet <caulet.theo@gmail.com>
2026-04-09 10:14:12 +0200

    Delete part4 directory

--- part4/backend/app/services/repositories/amenity_repository.py
+++
@@ -1,8 +0,0 @@
-from app.models.amenity import Amenity
-from app import db
-from app.persistence.repository import SQLAlchemyRepository
-
-
-class AmenityRepository(SQLAlchemyRepository):
-    def __init__(self):
-        super().__init__(Amenity)
--- part4/backend/app/services/repositories/review_repository.py
+++
@@ -1,8 +0,0 @@
-from app.models.review import Review
-from app import db
-from app.persistence.repository import SQLAlchemyRepository
-
-
-class ReviewRepository(SQLAlchemyRepository):
-    def __init__(self):
-        super().__init__(Review)
