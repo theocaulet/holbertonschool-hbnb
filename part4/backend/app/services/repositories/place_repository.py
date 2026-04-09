a11567c1ff52a68f81f56be3660002e8ba47aedb
theocaulet <caulet.theo@gmail.com>
2026-04-09 10:17:09 +0200

    Remove backend schema, test scripts, HTML files, JavaScript, and CSS for the HBnB application

--- part4/backend/app/services/repositories/place_repository.py
+++
@@ -1,8 +0,0 @@
-from app.models.place import Place
-from app import db
-from app.persistence.repository import SQLAlchemyRepository
-
-
-class PlaceRepository(SQLAlchemyRepository):
-    def __init__(self):
-        super().__init__(Place)
